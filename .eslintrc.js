0061c596fb8287eaa88a3546818e3ef822178816
Nick Milton <nmilton@coldfrontlabs.ca>
2019-12-10 09:40:16 -0500

    build(npm): add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    "standard",
+    "plugin:jsdoc/recommended",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["prettier", "jsdoc"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
