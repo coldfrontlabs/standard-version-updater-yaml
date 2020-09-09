145b65075951cee54445cb870c6efc8b24b9e3a4
Nick Milton <nmilton@coldfrontlabs.ca>
2020-09-09 12:00:31 -0400

    chore(eslint): apply autofixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,22 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: [
     "standard",
     "plugin:jsdoc/recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   plugins: ["prettier", "jsdoc"],
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
   },
   rules: {
-    "prettier/prettier": "error"
-  }
+    "prettier/prettier": "error",
+  },
 };
