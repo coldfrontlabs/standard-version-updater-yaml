b231d8a48adb3c1c82386cae6905c24d34451d58
Nick Milton <nmilton@coldfrontlabs.ca>
2021-09-27 14:18:55 -0400

    style: apply eslint and prettier autofixes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const yaml = require("yaml");
 
-module.exports.readVersion = contents => yaml.parse(contents, "utf8").version;
+// Get the current version in the yaml file.
+module.exports.readVersion = (contents) => yaml.parse(contents, "utf8").version;
 
+// Update the version in the yaml file.
 module.exports.writeVersion = (contents, version) => {
   const yamlFile = yaml.parse(contents, "utf8");
   yamlFile.version = version;
